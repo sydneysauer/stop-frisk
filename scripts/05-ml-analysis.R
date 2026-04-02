1e7b0d05dff55ce95784ace27608d12f4219225d
Sydney Sauer <sydney.sauer@outlook.com>
2026-04-02 14:52:44 -0400

    Test two approaches

--- scripts/05-ml-analysis.R
+++ scripts/05-ml-analysis.R
@@ -4,6 +4,7 @@ library(penalized)
 library(modelr)
 
 source(here("R/ml_functions.R"))
+set.seed(1235)
 
 train_data <- read_rds(here("data", "sqf_ml_train.rds"))
 holdout_data <- read_rds(here("data", "sqf_ml_holdout.rds"))
@@ -51,7 +52,7 @@ validation_set <- anti_join(train_data, train_set, by= "id")
 cat("Training set:  ", nrow(train_set), "rows\n")
 cat("Validation set: ", nrow(validation_set), "rows\n")
 
-# APPROACH 1: Theoretically informed model
+# Theoretically informed model
 # I think physical stature, time of day, race, gender, and violence will be most predictive.
 m1 <- glm(arrest ~ age + height + weight + hour + race + male + reason_violent, data = train_set, family = binomial)
 calculate_performance(m1, validation_set)
@@ -85,7 +86,11 @@ cross_validate(m2, train_data, k = 5)
 # IMPROVE PREDICTIONS (PART B)
 # ==============================================================================
 
-# FEATURE ENGINEERING
+# Goal here: Create three different modeling approaches and compare their performance.
+
+# ATTEMPT 1: FEATURE ENGINEERING EMPHASIS 
+# I think there are some transformations I can do to the data that will help the model learn better.
+
 # Create a new variable for whether the stop was in a high crime area (90th percentile+ stops)
 precinct_counts <- train_data %>%
   group_by(precinct) %>%
@@ -123,10 +128,47 @@ table(train_data$num_reasons) # Very few stops with more than 4 reasons, so I'll
 train_data <- train_data %>%
   mutate(num_reasons = ifelse(num_reasons > 4, 4, num_reasons))
 
+# Update the train_set, validation_set, and formula to include the new features.
+train_set      <- sample_frac(train_data, 0.7)
+validation_set <- anti_join(train_data, train_set, by= "id")
+cat("Training set:  ", nrow(train_set), "rows\n")
+cat("Validation set: ", nrow(validation_set), "rows\n")
+X  <- str_c(names(train_data)[!names(train_data) %in% c("arrest", "id")], collapse = " + ")
+f4 <- as.formula(str_c("arrest ~ ", X))
+m4 <- glm(f4, data = train_set, family = binomial)
 
+# Performance:
+appr1 <- cross_validate(m4, validation_set) 
+appr1 # Whoa! My new variables actually helped!
+compare_performance(bm, m4, train_data) # And much better than baseline!
 
-#TODO create a penfit performance function to replace calculate_accuracy, then I can call cross_validate_penfit
+# ATTEMPT 2: REGULARIZATION EMPHASIS 
+# Now, I'll take this new model with all the features and add regularization.
 
+# Start with basic lamba=10 and just a single train/validation split to get a baseline idea
+m4_lasso <- penalized(f4, lambda1 = 10, model = "logistic", data = train_set)
+appr2 <- cross_validate_penfit(train_data, f4, lambda = 10, k = 5) 
+appr2 # Even better than the non-regularized version! Let's continue in this direction.
+
+# Systematically search for optimal regularization strength
+models <- tibble(lambda = seq(1, 30, 3)) %>%
+  mutate(
+    performance = map(lambda, ~ cross_validate_penfit(train_data, f4, lambda = ., k = 5))
+  )
+# Look at the results
+results <- models %>% select(lambda, performance) %>% unnest_wider(performance)
+# Plot results to see a pattern
+ggplot(results, aes(x = lambda)) +
+  geom_line(aes(y = out_sample)) +
+  geom_point(aes(y = out_sample)) +
+  labs(x = "Lambda", y = "Out of Sample Log Loss", title = "Model Performance by Regularization Strength") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+
+# Ok, the graph here is telling me that lambda's optimal value was 1, which suggests to me that I don't have enough
+# coefficients! So, in my next approach, I'm going to add a whole bunch more features and interactions.
+
+# TODO: Officially deal with missing data before I add any new features. Re run all code to be certain!
 
 
 # ==============================================================================
@@ -135,4 +177,13 @@ train_data <- train_data %>%
 
 # Questions for Joscha:
 # 1. How to make sure all the transformations I did to the training data are also done to holdout? function with crazy validation?
+    # can recode the holdout data with the same exact code, since I have all the Xs
 # 2. What to do about missing values? I omitted them for now so I could run the code...
+    # remember: our goal is out of sample predictive performance!
+    # so if it seems useful, I can create new features based on missingness, or create a category of "unknown"
+    # it's actually OK to predict with separate models! (eg, I can predict NO ARREST for any missing hehe)
+      # justify this by saying that when I removed the missings, the % arrested went up substantially, meaning
+      # people with missing values are less likely to be arrested
+    # maybe even make a crosstab of missingness and arrest to show this?
+
+# TODO go back and edit my annotations based on now having the random seed set.
